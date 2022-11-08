0a909f9409f639c10f1c4ea0146970577e1e35fa
Iuliia Sokolovska <uu.sokil@gmail.com>
2022-11-08 17:09:21 +0100

    favoritecoctails

--- src/js/favoritecocktails.js
+++ src/js/favoritecocktails.js
@@ -2,30 +2,22 @@ import './sass/_favorite-cocktails.scss';
 import axios from 'axios';
 import { ApiService } from './apiservice';
 
-export default function createMarkUpCocktails(gallery, photos = []) {
-  let markUp = photos
-    .map(
-      ({
-        srtDrink,
-        srtDrinkThumb,
-      }) => `<h2 class="cocktails-title">Cocktails</h2>
-            <ul class="cocktails-list">
-            <li class="cocktails__item">
-                <img src="${srtDrinkThumb}" alt="${srtDrink} photo">
-                <h3 class="cocktails__name">${srtDrink}</h3>
-                <ul class="button-list">
-                    <li class="button__item">
-                        <button class="button-more" type="submit">Learn more
-                        </button>
-                    </li>
-                    <li class="button__item">
-                        <button class="button-add" type="submit">Add to
-                        </button>
-                    </li>
-                </ul>
-              </li>
-              </ul>`
-    )
-    .join('');
-  gallery.insertAdjacentHTML('afterend', markUp);
+const navSublinkCocktails = document.querySelector(
+  "a[href='./favorite-cocktails.html']"
+);
+
+navSublinkCocktails.addEventListener('click', showCoctails);
+
+export function showCoctails(e) {
+  if (e.target.tagName === navSublinkCocktails) {
+    console.log(e.target.href);
+  }
+
+  //   if (localStorage.getItem(DATA_KEY)) {
+  //     valueForm = JSON.parse(localStorage.getItem(DATA_KEY));
+  //     for (let key in valueForm) {
+  //       feedbackForm.elements[key].value = valueForm[key];
+  //     }
+  //   }
 }
+showCoctails();
