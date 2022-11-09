56909b6a40d64a4fa28e3f651e348190b3496d06
LeonidYezerskyi <111503058+LeonidYezerskyi@users.noreply.github.com>
2022-11-09 14:55:04 +0200

    start work

---
+++ src/js/heroSearchLetter.js
@@ -0,0 +1,9 @@
+import ApiService from './apiservice';
+
+const letterList = document.querySelector('.hero__navigation');
+const letterBtn = document.querySelector('.hero__button');
+const newsApi = new ApiService();
+
+letterList.addEventListener('click', showCoctails);
+
+function showCoctails() {}
