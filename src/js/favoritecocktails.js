27ec7ae29973485a76eb6e3cd6cd02513e80e4af
Iuliia Sokolovska <uu.sokil@gmail.com>
2022-11-09 23:34:58 +0100

    btnRemoveFAv

--- src/js/favoritecocktails.js
+++ src/js/favoritecocktails.js
@@ -1,22 +1,24 @@
 import ApiService from './apiservice';
 import { FAVORITE_KEY } from './cocktails';
+import Render from './render';
 const gallery = document.querySelector('.gallery');
 const galleryCocktailsTitle = document.querySelector(
   '.favorite-cocktails__title'
 );
+FAVORITE_KEY = 'favorite'; //del
 // const buttonMore = document.querySelector('.button-more');
 let apiService = new ApiService();
-
-// apiService.getCocktailRandom(3).then(data => {
-//   console.log(data);
-//   localStorage.setItem(FAVORITE_KEY, JSON.stringify(data));
-// });
+let render = new Render();
+apiService.getCocktailRandom(3).then(data => {
+  console.log(data);
+  localStorage.setItem(FAVORITE_KEY, JSON.stringify(data));
+});
 
 export function showAllCoctails(gallery) {
   let favoriteCocktails = JSON.parse(localStorage.getItem(FAVORITE_KEY));
   if (localStorage.getItem(FAVORITE_KEY)) {
     const markupCocktails = favoriteCocktails
-      .map(({ strDrink, strDrinkThumb }) => {
+      .map(({ strDrink, strDrinkThumb, idDrink }) => {
         return `<li class="cocktails__item">
                 <img class="cocktails__img" src="${strDrinkThumb}" alt="${strDrink} photo">
                 <h3 class="cocktails__name">${strDrink}</h3>
@@ -25,9 +27,8 @@ export function showAllCoctails(gallery) {
                         <button class="button-more" type="submit">Learn more
                         </button>
                     </li>
-                    <li class="button__item">
-                        <button class="button-add" type="submit">Add to
-                        </button>
+                  <li class="button__item">
+                     <button class='button-remove' type="button" data-id="${idDrink}" data-action="favorite">Remove</button>
                     </li>
                 </ul>
               </li>
@@ -45,3 +46,13 @@ export function showAllCoctails(gallery) {
 }
 
 showAllCoctails(gallery);
+
+const buttonRemove = document.querySelectorAll('.button-remove');
+console.log(buttonRemove);
+buttonRemove.forEach(elem =>
+  elem.addEventListener('click', e => {
+    console.dir(e.target.dataset.id);
+    // console.log(123);
+    render.addToFavorite(e.target.dataset.id);
+  })
+);
