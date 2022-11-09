b77dfc1981aad58f720ba10ca2037a9fa1c681d1
Nataliia Hlushchuk <hlushchuk.nataliia@gmail.com>
2022-11-09 14:49:46 +0300

    fix

--- src/js/cocktails.js
+++ src/js/cocktails.js
@@ -6,62 +6,16 @@ const gallery = document.querySelector('.cocktails-list');
 const container = document.querySelector('.container');
 const FAVORITE_KEY = 'favorite';
 const newsApi = new ApiService();
-// window.addEventListener('resize', changeMarkUpWithResize);
 
-// let render = new Render();
-// console.log(render);
-// render.markUpRandomCocktails();
-
-// async function markUpRandomCocktails() {
-//   let photo = await newsApi.getCocktailRandom(calculatePhoto());
-//   gallery.innerHTML = '';
-//   createMarkUpCocktails(gallery, photo);
-//   const btnAdd = document.querySelector('.button-add');
-//   btnAdd.addEventListener('click', addToFavorite);
-//   return photo;
-// }
 let photo = [];
-console.log(photo);
+
 // Функція відмальовки зображень
 async function markUpRandomCocktails() {
-  photo = await newsApi.getCocktailRandom(calculatePhoto());
-  console.log(photo);
+  photo = await newsApi.getCocktailRandom(9);
   gallery.innerHTML = '';
   createMarkUpCocktails(gallery, photo);
 }
 markUpRandomCocktails();
-// console.log(markUpRandomCocktails().then(res => console.log(res)));
-
-// Функція при зміні розміру вьюпорта
-// function changeMarkUpWithResize(e) {
-//   gallery.innerHTML = '';
-//   markUpRandomCocktails();
-// }
-// Функція розрахунку кількості фото від вьюпотру
-function calculatePhoto() {
-  const windowInnerWidth = window.innerWidth;
-  let count = 0;
-  if (windowInnerWidth > 0 && windowInnerWidth < 768) {
-    count = 3;
-  } else if (windowInnerWidth >= 768 && windowInnerWidth < 1280) {
-    count = 6;
-  } else {
-    count = 9;
-  }
-  return count;
-}
-// Функція яка ховає секції( якщо пусте значення падає)
-// async function sectionSelectionFoRender() {
-//   photo = await newsApi.getCocktailByFirstLetter('2');
-//   console.log(photo);
-//   if (photo.length === 0) {
-//     document.querySelector('.missingcoctails').classList.remove('.is-hidden');
-//     document.querySelector('.coctails').classList.add('.is-hidden');
-//     createMarkUpMissingCocktails(container);
-//   }
-//   createMarkUpCocktails(gallery, photo);
-// }
-// sectionSelectionFoRender();
 
 const cocktailsListRef = document.querySelector('.cocktails-list');
 cocktailsListRef.addEventListener('click', onClickBtn);
@@ -72,8 +26,7 @@ function onClickBtn(e) {
   console.log(id);
   addToFavorite(id);
 }
-
-// Функція додавання до улюблених
+// Функція додавання, видалення з улюблених
 function addToFavorite(id = '') {
   const dataLocalStorage = localStorage.getItem(FAVORITE_KEY);
   let favorite = dataLocalStorage ? JSON.parse(dataLocalStorage) : [];
@@ -87,3 +40,46 @@ function addToFavorite(id = '') {
   }
   localStorage.setItem(FAVORITE_KEY, JSON.stringify(favorite));
 }
+// Функція зміни стилів кнопки
+function changeBtn(id = '') {
+  const dataLocalStorage = localStorage.getItem(FAVORITE_KEY);
+  console.log(dataLocalStorage);
+  //   let favorite = dataLocalStorage ? JSON.parse(dataLocalStorage) : [];
+}
+
+// Функція яка ховає секції( якщо пусте значення падає)
+// async function sectionSelectionFoRender() {
+//   photo = await newsApi.getCocktailByFirstLetter('2');
+//   console.log(photo);
+//   if (photo.length === 0) {
+//     document.querySelector('.missingcocktails').classList.remove('.is-none');
+//     document.querySelector('.cocktails').classList.add('.is-none');
+//     createMarkUpMissingCocktails(container);
+//   }
+//   createMarkUpCocktails(gallery, photo);
+// }
+// sectionSelectionFoRender();
+
+// let render = new Render();
+// console.log(render);
+// render.markUpRandomCocktails();
+
+// window.addEventListener('resize', changeMarkUpWithResize);
+// / Функція розрахунку кількості фото від вьюпотру
+// function calculatePhoto() {
+//   const windowInnerWidth = window.innerWidth;
+//   let count = 0;
+//   if (windowInnerWidth > 0 && windowInnerWidth < 768) {
+//     count = 3;
+//   } else if (windowInnerWidth >= 768 && windowInnerWidth < 1280) {
+//     count = 6;
+//   } else {
+//     count = 9;
+//   }
+//   return count;
+
+// // Функція при зміні розміру вьюпорта
+// function changeMarkUpWithResize(e) {
+//   gallery.innerHTML = '';
+//   markUpRandomCocktails();
+// }
