15e0e3155552448b97eddeb4fd4ebe21c6ba68db
LeonidYezerskyi <111503058+LeonidYezerskyi@users.noreply.github.com>
2022-11-10 00:48:37 +0200

    make search coctails by letter work

--- src/js/heroSearchLetter.js
+++ src/js/heroSearchLetter.js
@@ -1,23 +1,33 @@
-import ApiService from './apiservice';
-import createMarkUpCocktails from './createMarkUpCocktails';
+import Render from './render';
 
 const letterList = document.querySelector('.hero__navigation');
 const letterBtn = document.querySelector('.hero__button');
 const gallery = document.querySelector('.cocktails-list');
 console.log(gallery);
 
-const newsApi = new ApiService();
-
+const render = new Render();
 letterList.addEventListener('click', showCoctails);
 
 async function showCoctails(event) {
-  console.log(event.target);
+  const refButton = document.querySelector('.hero__btn-active');
+  if (refButton) {
+    refButton.classList.remove('hero__btn-active');
+  }
+
   if (event.target.classList.contains('hero__button')) {
-    event.target.textContent;
-    const curArray = await newsApi.getCocktailByFirstLetter(
-      event.target.textContent
-    );
-    console.log(curArray);
-    createMarkUpCocktails(gallery, curArray);
+    render.sectionSelectionFoRender(event.target.textContent);
+    event.target.classList.add('hero__btn-active');
+  }
+  if (screen.availWidth < 768) {
+    const spanEl = document.querySelector('.hero__select-letter');
+    const heroNav = document.querySelector('.hero__navigation');
+    const heroSelect = document.querySelector('.hero__select');
+    const heroIcon = document.querySelector('.hero__icon');
+
+    spanEl.textContent = event.target.textContent;
+    heroNav.classList.add('is-hidden');
+    heroSelect.style.backgroundColor = '#FD5103';
+    spanEl.style.color = '#FCFCFC';
+    heroIcon.style.fill = '#FCFCFC';
   }
 }
