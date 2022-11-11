e92d5ff72533957b2196d4973b6f95f93c30ec7b
Iuliia Sokolovska <uu.sokil@gmail.com>
2022-11-11 14:27:33 +0100

    ingridient modal part

--- src/js/modalingr.js
+++ src/js/modalingr.js
@@ -1,21 +1,24 @@
 import Render, { FAVORITE_KEY } from './render';
+import ApiService from './apiservice';
 
-const modalIngridient = document.querySelector('.modal-ingridient');
-const backdrop = document.querySelector('.backdrop');
-const btnModalClose = document.querySelector('.modal-close');
+const modalIngridient = document.querySelector('.modal-content');
+const backdropIngridient = document.querySelector('.backdrop-ingridient');
+const btnModalClose = document.querySelector('#ingridient-close');
+
 let render = new Render();
+let apiService = new ApiService();
 
 export function openModalIngridient() {
-  backdrop.classList.remove('is-hidden');
+  backdropIngridient.classList.remove('is-hidden');
   btnModalClose.addEventListener('click', closeModalIngridient);
-  backdrop.addEventListener('click', onBackDropClick);
+  backdropIngridient.addEventListener('click', onBackDropClick);
   window.addEventListener('keydown', onEscClick);
 }
 
 export function closeModalIngridient() {
-  backdrop.classList.add('is-hidden');
+  backdropIngridient.classList.add('is-hidden');
   btnModalClose.removeEventListener('click', closeModalIngridient);
-  backdrop.removeEventListener('click', onBackDropClick);
+  backdropIngridient.removeEventListener('click', onBackDropClick);
   window.removeEventListener('keydown', onEscClick);
 }
 
@@ -29,4 +32,32 @@ function onEscClick(e) {
   if (e.code === 'Escape') {
     closeModalIngridient;
   }
+}
+
+export async function renderModalIngridient(ingridientName) {
+  const response = await apiService.getCocktailIngrName(ingridientName);
+  const {
+    idIngredient,
+    strDescription,
+    strIngredient,
+    strType,
+    strAlcohol,
+    strABV,
+  } = response[0];
+
+  let markup = `<h2>${strIngredient}</h2>
+<p>${strType ? strType : ''}</p>
+<p>${
+    strAlcohol === 'Yes'
+      ? `Alcohol by volume: ${strABV}%`
+      : `Alcohol: ${strAlcohol}`
+  }</p>
+<p>${
+    strDescription
+      ? strDescription
+      : `Sorry, we have no information about ${strIngredient}.`
+  }</p>
+ <button class='btn-remove' id='ingridient-btn' type="button" data-id="${idIngredient}">Remove from favorite</button>`;
+  console.log(response);
+  modalIngridient.innerHTML = markup;
 }
